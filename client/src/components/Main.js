897de6606adb21d1fd69219d26108031e85c62c8
Aya Sato <ayasatography@icloud.com>
2021-03-06 13:23:54 -0500

    Moved fetchToken to Main.js and added fetchAnimals

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { themeVars } from "./GlobalStyles";
+import { useSelector, useDispatch } from "react-redux";
+
+import { fetchToken, fetchAnimals } from "../helpers/api-helpers";
+import { isExpired } from "../reducers/auth-reducer";
+
+import Buttons from "./Buttons";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: calc(100vh - 100px);
+  background-color: ${themeVars.tintedWhite};
+`;
 
 const Main = () => {
-    return (
-        <div></div>
-    );
-}
+  const dispatch = useDispatch();
+  const accessToken = useSelector((state) => state.auth.token);
+  const location = "Montreal, Quebec";
+  const distance = 500;
+  const type = "cat";
+  const age = "young";
+
+  const isTokenExpired = useSelector((state) =>
+    isExpired(state.auth.expiresAt)
+  );
+
+  useEffect(() => {
+    if (!accessToken || isTokenExpired) {
+      fetchToken(dispatch);
+    } else {
+      fetchAnimals(dispatch, accessToken, location, distance, type, age);
+    }
+  }, [accessToken]);
+
+  return (
+    <Wrapper>
+      <Buttons />
+    </Wrapper>
+  );
+};
 
 export default Main;
---
+++ client/src/helpers/api-helpers.js
@@ -0,0 +1,43 @@
+import {
+  requestAccessToken,
+  receiveAccessToken,
+  receiveAccessTokenFailed,
+  requestPets,
+  receivePets,
+  receivePetsFailed,
+} from "../actions";
+
+export function fetchToken(dispatch) {
+  dispatch(requestAccessToken());
+  fetch("/petfinder_access_token")
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json, "token");
+      dispatch(receiveAccessToken(json.access_token));
+    })
+    .catch((err) => {
+      console.error(err);
+      dispatch(receiveAccessTokenFailed());
+    });
+}
+
+export function fetchAnimals(dispatch, token, location, distance, type, age) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const url = `https://api.petfinder.com/v2/animals?type=${type}&age=${age}&location=${location}&distance=${distance}&limit=100`;
+
+  dispatch(requestPets());
+  return fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json, "pets");
+      dispatch(receivePets(json));
+    })
+    .catch((err) => {
+      console.error(err);
+      dispatch(receivePetsFailed());
+    });
+}
