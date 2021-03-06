09c41e0d5e730d1f1e656587f93fdeb5f5d4812c
Aya Sato <ayasatography@icloud.com>
2021-03-06 13:18:36 -0500

    Added expiresAt to auth-reducer

--- client/src/reducers/auth-reducer.js
+++ client/src/reducers/auth-reducer.js
@@ -1,31 +1,35 @@
 const initialState = {
-    token: null,
-    status: "idle"
-}
+  token: null,
+  status: "idle",
+  expiresAt: null,
+};
 
 export default function authReducer(state = initialState, action) {
-    switch (action.type) {
-        case "REQUEST_ACCESS_TOKEN": {
-            return {
-                ...state,
-                status: "loading"
-            };
-        }
-        case "RECEIVE_ACCESS_TOKEN": {
-            return {
-                ...state,
-                token: action.token,
-                status: "idle"
-            };
-        }
-        case "RECEIVE_ACCESS_TOKEN_FAILED": {
-            return {
-                ...state,
-                status: "error"
-            };
-        }
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case "REQUEST_ACCESS_TOKEN": {
+      return {
+        ...state,
+        status: "loading",
+      };
     }
+    case "RECEIVE_ACCESS_TOKEN": {
+      return {
+        ...state,
+        token: action.token,
+        expiresAt: Date.now() + 3600,
+        status: "idle",
+      };
+    }
+    case "RECEIVE_ACCESS_TOKEN_FAILED": {
+      return {
+        ...state,
+        status: "error",
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 }
+
+export const isExpired = (expirationDate) => expirationDate <= Date.now();
