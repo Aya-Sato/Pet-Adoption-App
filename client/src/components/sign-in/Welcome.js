33e982dfdb991c9c52ff536b72bd6d1523a42dd7
Aya Sato <ayasatography@icloud.com>
2021-03-12 22:57:45 -0500

    Updated Welcome page

--- client/src/components/sign-in/Welcome.js
+++ client/src/components/sign-in/Welcome.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 import { auth } from "./Authentication";
 
 const Welcome = ({ currentUser, setCurrentUser }) => {
+  const history = useHistory();
+
   return (
     <>
       <div>
-        {currentUser === null
+        {currentUser === undefined
           ? "Welcome to Adopet!"
           : `Welcome to Adopet, ${currentUser}!`}
       </div>
       <button
         onClick={() => {
           auth.signOut();
-          setCurrentUser(null);
+          if (currentUser === undefined) {
+            history.push("/");
+          } else {
+            setCurrentUser(null);
+          }
         }}
       >
         SIGN OUT
