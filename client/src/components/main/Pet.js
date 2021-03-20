7c3a73ccc2510bf4e0c29cedbb05c42c5b0d4150
Aya Sato <ayasatography@icloud.com>
2021-03-20 11:27:07 -0400

    Added carousel to view multiple images of pet

--- client/src/components/main/Pet.js
+++ client/src/components/main/Pet.js
@@ -2,6 +2,15 @@ import React, { useContext } from "react";
 import { PetContext } from "./PetContext";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Img = styled.img`
+  width: 100%;
+  height: 80vh;
+  object-fit: cover;
+`;
 
 const Pet = () => {
   const { selectedPetId } = useContext(PetContext);
@@ -12,12 +21,27 @@ const Pet = () => {
     petsArr.find((pet) => {
       return pet.id === selectedPetId;
     });
+  const petPhotosArr =
+    findPet &&
+    findPet.photos.map((photo) => {
+      return photo.large;
+    });
 
-  console.log(findPet);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <>
-      <img src={findPet.photos[0] && findPet.photos[0].large} alt="pet" />
+      <Slider {...settings}>
+        {petPhotosArr.map((photo, index) => {
+          return <Img src={photo} alt="pet" key={index} />;
+        })}
+      </Slider>
     </>
   );
 };
