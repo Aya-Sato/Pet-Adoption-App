b201fc0c3e318d29a446c43531de4487dcca45db
Aya Sato <ayasatography@icloud.com>
2021-03-27 23:51:57 -0400

    Added credits page

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
@@ -10,9 +10,12 @@ import { PreferenceContext } from "./preference/PreferenceContext";
 import { IoMdSettings } from "react-icons/io";
 import { RiLogoutCircleRLine } from "react-icons/ri";
 import { CgNotes } from "react-icons/cg";
+import { ImCross } from "react-icons/im";
+import Dialog from "@material-ui/core/Dialog";
 
 const Wrapper = styled.div`
   width: 100vw;
+  height: calc(100vh - 100px);
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -61,6 +64,12 @@ const StyledBtn = styled.button`
     );
   }
 
+  &.modal {
+    position: absolute;
+    top: 20px;
+    right: 2px;
+  }
+
   &:active {
     transform: scale(1.1);
   }
@@ -103,11 +112,68 @@ const ProTip = styled.div`
   }
 `;
 
+const CreditsContainer = styled.div`
+  height: 100vh;
+  overflow: scroll;
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CreditsHeading = styled.h2`
+  font-size: 22px;
+  color: ${themeVars.green};
+  text-decoration: underline;
+`;
+
+const SubHeading = styled.h3`
+  font-size: 17px;
+  color: ${themeVars.darkGray};
+  width: 70%;
+  text-align: left;
+  margin-bottom: 0;
+`;
+
+const UL = styled.ul`
+  width: 70%;
+  margin-top: 8px;
+  font-size: 15px;
+`;
+
+const LI = styled.li`
+  color: ${themeVars.darkGray};
+  font-size: 15px;
+`;
+
+const Thanks = styled.p`
+  color: ${themeVars.darkGray};
+  width: 80%;
+  font-size: 15px;
+`;
+
+const Copyright = styled.div`
+  color: ${themeVars.darkGray};
+  margin: 20px 0 100px 0;
+  width: 80%;
+  text-align: center;
+  font-size: 15px;
+`;
+
 const Settings = () => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
   const currentUser = useSelector((state) => state.currentUser.currentUser);
   const { setPreference } = useContext(PreferenceContext);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Wrapper>
@@ -157,12 +223,7 @@ const Settings = () => {
           <BtnLabel>SETTINGS</BtnLabel>
         </BtnContainer>
         <BtnContainer>
-          <StyledBtn
-            className="small"
-            onClick={() => {
-              history.push("/credits");
-            }}
-          >
+          <StyledBtn className="small" onClick={handleClickOpen}>
             <CgNotes
               style={{
                 height: "23px",
@@ -177,6 +238,54 @@ const Settings = () => {
         </BtnContainer>
       </BtnsContainer>
       <ProTip>Pro tip: Swiping Right is the key to happiness!</ProTip>
+      <Dialog fullScreen open={open} onClose={handleClose}>
+        <StyledBtn className="small modal" onClick={handleClose}>
+          <ImCross
+            style={{
+              height: "23px",
+              width: "23px",
+              position: "relative",
+              top: "2px",
+              color: `${themeVars.coralOrange}`,
+            }}
+          />
+        </StyledBtn>
+        <CreditsContainer>
+          <CreditsHeading>Credits</CreditsHeading>
+          <SubHeading>APIs:</SubHeading>
+          <UL>
+            <LI>PetFinder API</LI>
+            <LI>Abstract IP Geolocation API</LI>
+            <LI>Stripe API</LI>
+          </UL>
+          <SubHeading>{`Authentication & Database:`}</SubHeading>
+          <UL>
+            <LI>Firebase</LI>
+          </UL>
+          <CreditsHeading>Special Thanks</CreditsHeading>
+          <Thanks>
+            First and foremost, I'd like to thank my instructor and the TCs from
+            the Concordia Bootcamp. Without their guidance and continuous
+            support throughout the Bootcamp, this project would not have been
+            possible. Thank you, Tiffany, Diana, Richard, and Bryce! You are the
+            best!!
+          </Thanks>
+          <Thanks>
+            {`I'd also like to thank my classmates Chris and Zayd. The friendship
+            we have built over the past 8 months has been invaluable to me. I
+            will certainly miss all the chats and video calls :)`}
+          </Thanks>
+          <Thanks>
+            Lastly, a big thank you to my mom, who has provided me with
+            tremendous support in my new journey to become a web developer.
+          </Thanks>
+          <CreditsHeading>Copyright</CreditsHeading>
+          <Copyright>
+            Copyright © 2021 Aya Sato, <br />
+            All Rights Reserved
+          </Copyright>
+        </CreditsContainer>
+      </Dialog>
     </Wrapper>
   );
 };
